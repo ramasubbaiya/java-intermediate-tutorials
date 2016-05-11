889495c5649cac57aac1dd5da1e8f8151cbfa97d
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2016-05-11 11:06:11 -0400

    Collections sorting

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # Java Intermediate Tutorials 
 :bowtie: Created by Rama
+
+:point_right: Lesson 1 : Recursion
+:point_right: Lesson 2 : Collections And ArrayList
+:point_right: Lesson 3 : LinkedList
+:point_right: Lesson 4 : List to Array
+:point_right: Lesson 5 : Collections sorting and replacing
---
+++ src/com/tutorial/rama/Lesson5CollectionSorting.java
@@ -0,0 +1,38 @@
+package com.tutorial.rama;
+
+import java.util.*;
+
+public class Lesson5CollectionSorting {
+
+	public static void main(String[] args) {
+		String[] things = {"eraser", "pencil", "scale", "tin", "can", "book"};
+		List<String> list = Arrays.asList(things);
+		System.out.printf(" %s", list);
+		System.out.println();
+				
+		// Sorting in Ascending order
+		Collections.sort(list);
+		System.out.printf(" %s", list);
+		System.out.println();
+		
+		// Sorting in Descending order
+		Collections.reverse(list);
+		System.out.printf(" %s", list);
+		System.out.println();
+
+		Collections.replaceAll(list, "tin", "big_box");
+		System.out.printf(" %s", list);
+		
+		
+		/*Sample Output
+		 * 
+		 *   [eraser, pencil, scale, tin, can, book]
+		 *   [book, can, eraser, pencil, scale, tin]
+		 *   [tin, scale, pencil, eraser, can, book]
+		 *   [big_box, scale, pencil, eraser, can, book]
+		 *   			 
+		 * */
+		
+	}
+
+}
