a6a496113be5351c8a9dc1d0e302cf7ddc71dacc
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2016-05-15 14:20:51 -0400

    stack push pop peek

--- src/com/tutorial/rama/Lesson8StacksPushPop.java
+++ src/com/tutorial/rama/Lesson8StacksPushPop.java
@@ -1,10 +1,30 @@
 package com.tutorial.rama;
 
+import java.util.*;
+
 public class Lesson8StacksPushPop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Stack<String> stack = new Stack<String>();
+		stack.push("item 1");
+		stack.push("item 2");
+		
+		//Print the stack
+		System.out.println(stack);
+		// Output
+		// [item 1, item 2]
+		
+		// Check the top of the stack
+		System.out.println(stack.peek());
+		// Output
+		// item 2
+		
+		//Remove the object that is in th top of the stack 
+		stack.pop();  
+		System.out.println(stack);
+		// Output
+		// [item 1]
 	}
 
 }
