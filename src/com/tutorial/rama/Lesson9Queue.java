c1b64c858c2d4014f8fe4d94beddb439503bb0b6
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2016-05-16 16:32:48 -0400

    queue

--- src/com/tutorial/rama/Lesson9Queue.java
+++ src/com/tutorial/rama/Lesson9Queue.java
@@ -1,10 +1,34 @@
 package com.tutorial.rama;
 
+import java.util.*;
+
 public class Lesson9Queue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PriorityQueue<String> queue = new PriorityQueue<String>();
+		
+		queue.offer("first");
+		queue.offer("second");
+		queue.offer("third");
+		
+		System.out.printf("%s ", queue);
+		System.out.println();
+		// Output
+		// [first, second, third] 
+		
+		//Head of the queue 
+		System.out.printf("%s ", queue.peek());
+		System.out.println();
+		// Output
+		// first
+		
+		//Remove the head of the queue, here it removes first element
+		queue.poll();
+		
+		//Print the queue after removal
+		System.out.printf("%s ", queue);
+		// Output
+		// [second, third] 
 	}
 
 }
